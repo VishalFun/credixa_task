ad9026e3a2c4478b5a46d158c3e630d3a7f7eae4
VishalFun <vs0032532@gmail.com>
2020-04-10 19:42:35 +0530

    setting changed

--- credicxo_task/settings.py
+++ credicxo_task/settings.py
@@ -137,4 +137,7 @@ USE_TZ = True
 ROOT_PATH = os.path.dirname(os.path.abspath(__file__)) 
 STATIC_URL = '/static/'
 STATIC_ROOT  =  os.path.join(BASE_DIR,'static')
-#STATICFILES_DIRS = ( os.path.join(BASE_DIR, 'static'), )
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/var/www/static/',
+]
