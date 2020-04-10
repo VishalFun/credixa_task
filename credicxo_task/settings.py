d8d3f052e8ef07e3f883d8b9443430d9dabd48d1
VishalFun <vs0032532@gmail.com>
2020-04-10 19:28:21 +0530

    setting changed

--- credicxo_task/settings.py
+++ credicxo_task/settings.py
@@ -136,5 +136,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 ROOT_PATH = os.path.dirname(os.path.abspath(__file__)) 
 STATIC_URL = '/static/'
-STATIC_ROOT  =  os.path.join(ROOT_PATH,'static')
-STATICFILES_DIRS = ( os.path.join(BASE_DIR, 'static'), )
+STATIC_ROOT  =  os.path.join(BASE_DIR,'static')
+#STATICFILES_DIRS = ( os.path.join(BASE_DIR, 'static'), )
