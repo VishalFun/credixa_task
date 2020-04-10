eb0609145fc24bbce7adc954fbae061ec296d7cf
VishalFun <vs0032532@gmail.com>
2020-04-10 18:52:15 +0530

    setting changed

--- credicxo_task/settings.py
+++ credicxo_task/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'roiv@lybmk(5rn1ebm&bs#5(g3oekzm#9$ke%agc^)j1xq8o8('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['credicxotask.herokuapp.com']
 
