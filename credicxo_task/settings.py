30cd7dcd8e6af825681a41f9a69f3a11605b9134
VishalFun <vs0032532@gmail.com>
2020-04-11 14:11:54 +0530

    setting changed

--- credicxo_task/settings.py
+++ credicxo_task/settings.py
@@ -138,8 +138,6 @@ USE_TZ = True
 ROOT_PATH = os.path.dirname(os.path.abspath(__file__)) 
 STATIC_URL = '/static/'
 STATIC_ROOT  =  os.path.join(BASE_DIR,'static')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
